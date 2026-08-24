56872259c1ffebd447a7c54bf474de59aed96898
Panda Lee <pandaleecn@gmail.com>
2026-08-24 09:59:12 +0800

    cc-pocket 1.9.0: bump version + both arch daemon tarball sha256

--- Casks/cc-pocket.rb
+++ Casks/cc-pocket.rb
@@ -10,9 +10,9 @@ cask "cc-pocket" do
   # bit-reproducible, so even the arm64 sha changes on a rebuild).
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.8.1"
-  sha256 arm:   "f6efc43b8372a39056c5acb18e2da689a235c86aecfa91590f1336e46cc5cc02",
-         intel: "6c504ee546bfab5936791d7732fa861a4047e410a9b1555afaf929fae714a1dd"
+  version "1.9.0"
+  sha256 arm:   "396260b37035c3e0f08219d26d7b1457e9cc70e2c8762194534c6add2bfc1980",
+         intel: "44bc7048a520fc7edaa7dda3c4b92092ae91c831c34710d3df38999ecfbd52a3"
 
   url "https://github.com/heypandax/cc-pocket/releases/download/v#{version}/cc-pocket-daemon-#{version}-macos-#{arch}.tar.gz"
   name "CC Pocket daemon"
